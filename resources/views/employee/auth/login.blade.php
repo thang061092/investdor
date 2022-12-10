931ee19b32f3a53ff1c1744eb8f38b15be4bae20
Bui Manh Thang <buimanhthangtb@gmail.com>
2022-12-10 14:55:19 +0700

    update

--- resources/views/employee/auth/login.blade.php
+++ resources/views/employee/auth/login.blade.php
@@ -5,6 +5,7 @@
     <link type=”image/x-icon” href="{{asset('frontend/images/logo.png')}}" rel="shortcut icon"/>
     <script src="{{ asset('js/tabler.min.js') }}"></script>
     <script src="https://www.google.com/recaptcha/api.js"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
         rel="stylesheet">
