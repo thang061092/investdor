dbd35afcd2293f4936b630282fdcb922c368cb30
Bui Manh Thang <buimanhthangtb@gmail.com>
2022-12-11 22:03:22 +0700

    upadte

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/admin')->group(function () {
+    Route::post('/create_admin', "Admin\UserController@create_admin")->name('admin.create_admin');
+});
