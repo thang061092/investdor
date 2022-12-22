0d08e73c1d00b26b315d025965c2a329962bd6f9
Bui Manh Thang <buimanhthangtb@gmail.com>
2022-12-22 22:03:34 +0700

    update

--- resources/views/customer/home/detail-project.blade.php
+++ resources/views/customer/home/detail-project.blade.php
@@ -166,7 +166,7 @@
     @endforeach
     @if(!in_array($project->status, [1,3,4,5]))
         <a href="{{route('investment.step1',['slug'=> $project->slug_vi])}}" title="{{__('project.invest_now')}}"
-           class="btn_all lg mx-auto invest-now">{{__('project.invest_now')}}
+           class="btn_all lg mx-auto invest-now" style="margin-top: 20px">{{__('project.invest_now')}}
             <svg class="ml-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                 <path fill-rule="evenodd" clip-rule="evenodd"
                       d="M3.33398 9.99935C3.33398 9.53911 3.70708 9.16602 4.16732 9.16602H15.834C16.2942 9.16602 16.6673 9.53911 16.6673 9.99935C16.6673 10.4596 16.2942 10.8327 15.834 10.8327H4.16732C3.70708 10.8327 3.33398 10.4596 3.33398 9.99935Z"
